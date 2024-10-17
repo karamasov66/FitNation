ebaa2e60b561fe3f5bd96a153579181d9a2b290d
Sidi <sidimed.sidimed.147@gmail.com>
2024-10-17 10:14:22 +0200

    end footer

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('menuToggle').addEventListener('click', function() {
+    const navbar = document.getElementById('navbar');
+    navbar.style.display = (navbar.style.display === 'block') ? 'none' : 'block';
+});
